90bbf821ac2b6226bea9d86cf570a5143f5971b1
Chiraag Nadig <79017920+ChiraagNadig@users.noreply.github.com>
2024-07-21 21:02:26 -0400

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -7,6 +7,49 @@ menuIcon.onclick = () => {
     navbar.classList.toggle('active')
 };
 
+const coords = { x: 0, y: 0 };
+const circles = document.querySelectorAll(".circle");
+
+const colors = [
+    "#6c18f4", "#7b31f4", "#8943f5", "#9654f5", "#a164f5", "#ac73f6", "#b682f6", "#bf91f5", "#c8a0f5", "#d0aef5", "#d8bdf5", "#e0ccf4"
+];
+
+circles.forEach(function (circle, index) {
+  circle.x = 0;
+  circle.y = 0;
+  circle.style.backgroundColor = colors[index % colors.length];
+});
+
+window.addEventListener("mousemove", function(e){
+  coords.x = e.clientX;
+  coords.y = e.clientY;
+  
+});
+
+function animateCircles() {
+  
+  let x = coords.x;
+  let y = coords.y;
+  
+  circles.forEach(function (circle, index) {
+    circle.style.left = x - 12 + "px";
+    circle.style.top = y - 12 + "px";
+    
+    circle.style.scale = (circles.length - index) / circles.length;
+    
+    circle.x = x;
+    circle.y = y;
+
+    const nextCircle = circles[index + 1] || circles[0];
+    x += (nextCircle.x - x) * 0.3;
+    y += (nextCircle.y - y) * 0.3;
+  });
+ 
+  requestAnimationFrame(animateCircles);
+}
+
+animateCircles();
+
 /*================================= scroll section active link ===================================*/
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
